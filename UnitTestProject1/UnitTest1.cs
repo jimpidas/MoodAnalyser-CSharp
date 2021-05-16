212ceea76a7ff87dae1e16a1b6407509c413bd92
unknown <jimpidas>
2021-05-16 18:18:07 +0530

    [Jyosmita]Added: Mood anlyser code with Exeption handling

---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MoodAnalyser1;
+using System;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void GivenMoodAnalyseClassName_ShouldReturnMoodAnalyseObject()
+        {
+            string message = null;
+            object expected = new MoodAnalyser(message);
+            object obj = MoodAnalyserFactory.CreateMoodAnalyser("MoodAnalyser1.MoodAnalyser", "MoodAnalyser");
+            expected.Equals(obj);
+            
+        }
+        [TestMethod]
+        public void GivenMoodAnalyseClassName_ShouldReturnMoodAnalyseObject_UsingParameterizedConstructor()
+        {
+            object expected = new MoodAnalyser("HAPPY");
+            object obj = MoodAnalyserFactory.CreateMoodAnalyseUsingParameterizedConstructor("MoodAnalyser1.MoodAnalyser", "MoodAnalyser", "SAD");
+            expected.Equals(obj);
+        }
+
+        [TestMethod]
+        public void GivenMoodHappy_ShouldReturnHappy()
+        {
+            MoodAnalyser obj = new MoodAnalyser("HAPPY");
+            string result = obj.analyseMood();
+            Assert.AreEqual("HAPPY", result);
+
+        }
+        [TestMethod]
+        [ExpectedException(typeof(MoodAnalyserCustomException))]
+        public void GivenMoodNull_ShouldThrowException()
+        {
+            MoodAnalyser obj = new MoodAnalyser(null);
+            string result = obj.analyseMood();
+          
+
+        }
+        [TestMethod]
+        [ExpectedException(typeof(MoodAnalyserCustomException))]
+        public void GivenMoodEmpty_ShouldThrowException()
+        {
+            MoodAnalyser obj = new MoodAnalyser("");
+            string result = obj.analyseMood();
+           
+
+        }
+
+
+    }
+
+}
